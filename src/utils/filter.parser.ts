b060c3eaf80d2d332ee0eefbef490d4d50162a40
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-11-23 17:20:25 +0700

    feat: commet parse

---
+++ src/utils/filter.parser.ts
@@ -0,0 +1,11 @@
+// export class FilterParser {
+//   static parse(filter: string) {
+//     if (!filter) return [];
+
+//     const tokens = filter.replace(/\s+/g, '').split(/ (and|or) /i);
+
+//     const conditions = [];
+
+//     for (let i = 0; i < tokens.length === 'and' || )
+//   }
+// }
