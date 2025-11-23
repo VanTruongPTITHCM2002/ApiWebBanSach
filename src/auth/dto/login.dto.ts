742a6d56d09d0f0a27473306006c4996ec13bddf
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-11-23 19:15:39 +0700

    feat(login): add remeberme in dto login

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,5 +1,12 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class LoginDto {
   @ApiProperty({ example: 'user123', description: 'Tên đăng nhập' })
@@ -14,4 +21,9 @@ export class LoginDto {
   @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
   @MinLength(1, { message: 'Mật khẩu phải có ít nhất 1 ký tự' })
   password: string;
+
+  @ApiPropertyOptional({ description: 'Remember me', example: true })
+  @IsOptional()
+  @IsBoolean()
+  rememberMe?: boolean;
 }
