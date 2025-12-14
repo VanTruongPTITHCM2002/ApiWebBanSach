ed870da5d221eb5a0b2935fa4c57b05990052cc9
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-12-14 15:03:07 +0700

    feat(accounts): refactor-import-accounts-module

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -3,8 +3,8 @@ import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './entities/account.entity';
-import { Role } from 'src/roles/entities/role.entity';
-import { User } from 'src/users/entities/user.entity';
+import { Role } from '@/roles/entities/role.entity';
+import { User } from '@/users/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Account, Role, User])],
