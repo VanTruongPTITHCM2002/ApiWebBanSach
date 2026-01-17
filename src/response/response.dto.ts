59b11857748a5059b724263a7fe91fec77dc205b
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-17 12:57:52 +0700

    feat(*):add-success

--- src/response/response.dto.ts
+++ src/response/response.dto.ts
@@ -1,50 +1,51 @@
 export class ApiRes<T> {
   constructor(
-    public statusCode: number,
-    public message: string,
+    public success?: boolean,
+    public statusCode?: number,
+    public message?: string,
     public data?: T,
     public error?: string | object,
   ) {}
 
   static success<T>(message: string, data?: T): ApiRes<T> {
-    return new ApiRes<T>(200, message, data);
+    return new ApiRes<T>(true, 200, message, data, null);
   }
 
   static error<T>(message: string, error: string = 'Thất bại'): ApiRes<T> {
-    return new ApiRes<T>(400, message, undefined, error);
+    return new ApiRes<T>(false, 400, message, undefined, error);
   }
 
   static unauthorized<T>(
     message: string,
     error: string = 'Thất bại',
   ): ApiRes<T> {
-    return new ApiRes<T>(401, message, undefined, error);
+    return new ApiRes<T>(false, 401, message, undefined, error);
   }
 
   static forbidden<T>(message: string, error: string = 'Thất bại'): ApiRes<T> {
-    return new ApiRes<T>(403, message, undefined, error);
+    return new ApiRes<T>(false, 403, message, undefined, error);
   }
 
   static notFound<T>(message: string, error: string = 'Thất bại'): ApiRes<T> {
-    return new ApiRes<T>(404, message, undefined, error);
+    return new ApiRes<T>(false, 404, message, undefined, error);
   }
 
   static conflict<T>(message: string, error: string = 'Thất bại'): ApiRes<T> {
-    return new ApiRes<T>(409, message, undefined, error);
+    return new ApiRes<T>(false, 409, message, undefined, error);
   }
 
   static internalServerError<T>(
     message: string,
     error: string = 'Thất bại',
   ): ApiRes<T> {
-    return new ApiRes<T>(500, message, undefined, error);
+    return new ApiRes<T>(false, 500, message, undefined, error);
   }
 
   static badRequest<T>(message: string, error: string = 'Thất bại'): ApiRes<T> {
-    return new ApiRes<T>(400, message, undefined, error);
+    return new ApiRes<T>(false, 400, message, undefined, error);
   }
 
   static created<T>(message: string, data?: T): ApiRes<T> {
-    return new ApiRes<T>(201, message, data);
+    return new ApiRes<T>(true, 201, message, data);
   }
 }
