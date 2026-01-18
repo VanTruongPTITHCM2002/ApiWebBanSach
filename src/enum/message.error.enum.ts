f2c0ada757286305f33cbc52fceeef4c054a489d
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-18 13:58:34 +0700

    feat(*):add-some-enum-error

--- src/enum/message.error.enum.ts
+++ src/enum/message.error.enum.ts
@@ -1,4 +1,6 @@
 export enum MessageError {
   INTERNAL_SERVER_ERROR = 'Đã có lỗi xảy ra trong hệ thống',
   USERNAME_NOT_FOUND = 'Không tìm thấy tên tài khoản',
+  USER_NOT_LOGIN = 'Vui lòng đăng nhập để thực hiện thao tác',
+  USER_INCORRECT = 'Tài khoản hoặc mật khẩu không đúng',
 }
