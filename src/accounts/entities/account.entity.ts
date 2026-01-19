7aa3b46b37198c8521277349120530210dba87e4
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-19 16:26:49 +0700

    feat(account):add-extend-base-entity

--- src/accounts/entities/account.entity.ts
+++ src/accounts/entities/account.entity.ts
@@ -1,9 +1,9 @@
+import { BaseEntity } from '@/common/base.entity';
 import { Review } from '@/reviews/entities/review.entity';
 import { Role } from '@/roles/entities/role.entity';
 import { User } from '@/users/entities/user.entity';
 import {
   Column,
-  CreateDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
@@ -13,7 +13,7 @@ import {
 } from 'typeorm';
 
 @Entity('accounts')
-export class Account {
+export class Account extends BaseEntity {
   @PrimaryGeneratedColumn()
   accountId: number;
 
@@ -26,16 +26,10 @@ export class Account {
   @Column({ length: 255 })
   password: string;
 
-  @Column()
-  status: boolean;
-
   @ManyToOne(() => Role, (role) => role.accounts)
   @JoinColumn({ name: 'roleId' })
   roleId: Role;
 
   @OneToMany(() => Review, (review) => review.accountName)
   reviewlst: Review[];
-
-  @CreateDateColumn()
-  createdAt: Date;
 }
