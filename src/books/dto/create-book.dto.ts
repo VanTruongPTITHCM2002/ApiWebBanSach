6ecf2fda33b01f8d91b625e8c1f7680466642198
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-04 17:23:52 +0700

    feat(book):update-book-create-dto

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,35 +1,38 @@
-import { IsNotEmpty, IsOptional, MaxLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  IsUUID,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateBookDto {
-  // @IsDefined({ message: 'Không được phép bỏ qua tiêu đề' })
   @IsNotEmpty({ message: 'Không được phép bỏ trống tiêu đề' })
   @MaxLength(45, { message: 'Độ dài tiêu đề tối đa là 45 kí tự' })
   title: string;
 
-  @IsNotEmpty({ message: 'Không được phép để trống tên tác giả' })
-  // @IsDefined({ message: 'Không được phép bỏ qua tên tác giả' })
-  @MaxLength(45, { message: 'Độ dài tên của tác giả tối đa là 45 kí tự' })
-  authorName: string | number;
+  @IsNotEmpty({ message: 'Không được phép để trống tác giả' })
+  @IsUUID('4', { message: 'Mã tác giả sai định dạng' })
+  authorId: string;
 
   @IsNotEmpty({ message: 'Không được phép để trống tên nhà xuất bản' })
-  // @IsDefined({ message: 'Không được phép bỏ qua tên nhà xuất bản' })
-  @MaxLength(45, { message: 'Độ dài của nhà xuất bản tối đa 45 kí tự' })
-  publisherName: string | number;
+  @IsUUID('4', { message: 'Mã nhà xuất bản sai định dạng' })
+  publisherId: string;
 
   @IsNotEmpty({ message: 'Không được phép để trống tên danh mục sách' })
-  // @IsDefined({ message: 'Không được phép bỏ qua tên danh mục sách' })
-  @MaxLength(45, { message: 'Độ dài của tên danh mục sách tôi đa 45 kí tự' })
-  categoryName: string | number;
+  @IsUUID('4', { message: 'Mã thể loại sai định dạng' })
+  categoryId: string;
 
   @IsNotEmpty({ message: 'Không được phép để trống giá sách' })
-  // @IsDefined({ message: 'Không được phép bỏ qua giá sách' })
-  // @IsNumber({}, { message: 'Giá sách phải là số' })
+  // @IsPositive({ message: 'Giá phải lớn hơn 0' })
   price: number;
 
   @IsNotEmpty({ message: 'Không được phép để trống số lượng sách' })
-  // @IsDefined({ message: 'Không được phép bỏ qua số lượng sách' })
-  // @IsNumber({}, { message: 'Số lượng sách phải là số' })
+  // @IsPositive({ message: 'Số lượng phải lớn hơn 0' })
   stock: number;
+
+  @IsOptional()
+  files: File[];
 
   @IsOptional()
   link?: string;
