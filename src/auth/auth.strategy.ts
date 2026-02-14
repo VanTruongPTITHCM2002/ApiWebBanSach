14dcdb00665e14f7a8b096649a56ad32cf75412a
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-14 15:51:49 +0700

    refactor(*):remove-comment

--- src/auth/auth.strategy.ts
+++ src/auth/auth.strategy.ts
@@ -7,7 +7,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.SECRET_KEY, // Thay đổi thành khóa bí mật của bạn
+      secretOrKey: process.env.SECRET_KEY,
     });
   }
 
