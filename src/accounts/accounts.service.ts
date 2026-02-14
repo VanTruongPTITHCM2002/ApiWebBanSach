54f4569e0221f275c4aff69ed454a640e971714a
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-14 15:50:36 +0700

    refactor(account):remove-import-uneccessary-in-service

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
-import { UpdateAccountDto } from './dto/update-account.dto';
+import { UpdateAccountDto } from '@/accounts/dto/update-account.dto';
 import { Account } from './entities/account.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
