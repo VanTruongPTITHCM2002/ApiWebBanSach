fc775bce93744e77cc4cca0174059f4b0e272f20
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-14 15:39:13 +0700

    feat(auth):update-login-dto-add-define-change-length

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsBoolean,
+  IsDefined,
   IsNotEmpty,
   IsOptional,
   IsString,
@@ -10,6 +11,7 @@ import {
 
 export class LoginDto {
   @ApiProperty({ example: 'user123', description: 'Tên đăng nhập' })
+  @IsDefined({ message: 'Tên đăng nhập là bắt buộc' })
   @IsString({ message: 'Tên đăng nhập phải là chuỗi' })
   @IsNotEmpty({ message: 'Tên đăng nhập không được để trống' })
   @MinLength(3, { message: 'Tên đăng nhập phải có ít nhất 3 ký tự' })
@@ -17,9 +19,10 @@ export class LoginDto {
   username: string;
 
   @ApiProperty({ example: 'mypassword', description: 'Mật khẩu' })
+  @IsDefined({ message: 'Mật khẩu là bắt buộc' })
   @IsString({ message: 'Mật khẩu phải là chuỗi' })
   @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
-  @MinLength(1, { message: 'Mật khẩu phải có ít nhất 1 ký tự' })
+  @MinLength(8, { message: 'Mật khẩu phải có ít nhất 8 ký tự' })
   password: string;
 
   @ApiPropertyOptional({ description: 'Remember me', example: true })
