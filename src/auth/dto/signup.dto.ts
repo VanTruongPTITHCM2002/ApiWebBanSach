1499aff5861f74d07f9486ae6c092c45ce1e05b1
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-14 15:41:59 +0700

    feat(auth):add-is-defined-and-is-strong-password-in-sign-up-dto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,21 +1,43 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class SignUpDto {
   @ApiProperty({ example: 'user123', description: 'Tên đăng nhập' })
+  @IsDefined({ message: 'Tên đăng nhập là bắt buộc' })
   @IsString({ message: 'Tên đăng nhập phải là chuỗi' })
   @IsNotEmpty({ message: 'Tên đăng nhập không được để trống' })
   @MinLength(3, { message: 'Tên đăng nhập phải có ít nhất 3 ký tự' })
   @MaxLength(40, { message: 'Tên đăng nhập tối đa 40 ký tự' })
   username: string;
   @ApiProperty({ example: 'mypassword', description: 'Mật khẩu' })
+  @IsDefined({ message: 'Mật khẩu là bắt buộc' })
+  @IsStrongPassword({
+    minLength: 8,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
   @IsString({ message: 'Mật khẩu phải là chuỗi' })
   @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
-  @MinLength(6, { message: 'Mật khẩu phải có ít nhất 6 ký tự' })
+  @MinLength(8, { message: 'Mật khẩu phải có ít nhất 8 ký tự' })
   password: string;
   @ApiProperty({ example: 'mypassword', description: 'Mật khẩu' })
   @IsString({ message: 'Mật khẩu phải là chuỗi' })
   @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
-  @MinLength(6, { message: 'Mật khẩu phải có ít nhất 6 ký tự' })
+  @MinLength(8, { message: 'Mật khẩu phải có ít nhất 8 ký tự' })
+  @IsStrongPassword({
+    minLength: 8,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
+  @IsDefined({ message: 'Mật khẩu nhập lại là bắt buộc' })
   repassword: string;
 }
