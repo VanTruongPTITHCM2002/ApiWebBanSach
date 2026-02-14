d945007a4fc72a17078249a6d805f12f498de458
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-14 15:41:10 +0700

    feat(*):add-message-in-enum-error

--- src/enum/message.error.enum.ts
+++ src/enum/message.error.enum.ts
@@ -3,4 +3,6 @@ export enum MessageError {
   USERNAME_NOT_FOUND = 'Không tìm thấy tên tài khoản',
   USER_NOT_LOGIN = 'Vui lòng đăng nhập để thực hiện thao tác',
   USER_INCORRECT = 'Tài khoản hoặc mật khẩu không đúng',
+  PASSWORD_NOT_MATCH = 'Mật khẩu không trùng khớp',
+  USER_EXISTED = 'Tài khoản đã tồn tại',
 }
