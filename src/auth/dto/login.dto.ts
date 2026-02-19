197dbdf121e25c3481f01f1cc36f1d5436b6f36f
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-19 08:45:27 +0700

    feat(auth):update-login-dto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -5,8 +5,6 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
-  MaxLength,
-  MinLength,
 } from 'class-validator';
 
 export class LoginDto {
@@ -14,15 +12,12 @@ export class LoginDto {
   @IsDefined({ message: 'Tên đăng nhập là bắt buộc' })
   @IsString({ message: 'Tên đăng nhập phải là chuỗi' })
   @IsNotEmpty({ message: 'Tên đăng nhập không được để trống' })
-  @MinLength(3, { message: 'Tên đăng nhập phải có ít nhất 3 ký tự' })
-  @MaxLength(40, { message: 'Tên đăng nhập tối đa 40 ký tự' })
   username: string;
 
   @ApiProperty({ example: 'mypassword', description: 'Mật khẩu' })
   @IsDefined({ message: 'Mật khẩu là bắt buộc' })
   @IsString({ message: 'Mật khẩu phải là chuỗi' })
   @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
-  @MinLength(8, { message: 'Mật khẩu phải có ít nhất 8 ký tự' })
   password: string;
 
   @ApiPropertyOptional({ description: 'Remember me', example: true })
